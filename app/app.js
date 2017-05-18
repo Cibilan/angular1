77cd015a1e0c8e7fe34c3487f5d98b748a1830bf
cibilan <cibilanj@babajob.com>
2017-05-18 19:30:20 +0530

    button redirect

--- app/app.js
+++ app/app.js
@@ -1,6 +1,7 @@
-var pilotApp = angular.module('pilotApp', ['ngRoute']);
+var pilotApp = angular.module('pilotApp', ['ngRoute','dataGrid','pagination','ngMaterial']);
 
 pilotApp.config(['$routeProvider', function($routeProvider){
+	
 	$routeProvider
 		.when('/home', {
 			templateUrl: 'views/home.html',
@@ -10,24 +11,28 @@ pilotApp.config(['$routeProvider', function($routeProvider){
 			templateUrl: 'views/assetList.html',
 			controller: 'assetListController'
 		})
-		.when('/contractList', {
+		.when('/orderList', {
 			templateUrl: 'views/contractList.html',
 			controller: 'contractListController'
 		})
-		.when('/contractDetails', {
+		.when('/contractDetails/:id', {
 			templateUrl: 'views/contractDetails.html',
 			controller: 'contractDetailsController'
 		})
-		.otherwise({
-			redirectTo: '/home'
+		.when('/createOrder', {
+			templateUrl: 'views/createOrder.html',
+			controller: 'createOrderController'
 		})
+		.otherwise({
+			redirectTo: '/orderList'
+		});
 }]);
 
 pilotApp.controller('mainController', ['$scope' , function($scope) {
 
 }]);
 
-pilotApp.controller('assetListController', ['$scope', '$http' , function($scope, $http) {
+pilotApp.controller('assetListController', ['$scope', '$http', function($scope, $http) {
 
 	$http.get('data/assestListResponse.json').success(function(reponse){
 		console.log(reponse);
@@ -37,22 +42,44 @@ pilotApp.controller('assetListController', ['$scope', '$http' , function($scope,
 
 }]);
 
-pilotApp.controller('contractListController', ['$scope', '$http' , function($scope, $http) {
+pilotApp.controller('contractListController', ['$scope', '$http' , '$location', function($scope, $http, $location) {
 
-	$http.get('data/contractListResponse.json').success(function(reponse){
+		$scope.gridOptions = {
+            data: [],
+            urlSync: false
+        };
+
+	$http.get('data/dispatchOrderList.json').success(function(reponse){
 		console.log(reponse);
-		$scope.contractList = JSON.parse(reponse.result.message);
-		console.log($scope.contractList);
+		var message = reponse.result.message;
+		var message1 = message.replace	("}{","},{");
+		var message2 = "[" + message1 + "]";
+		console.log(message2);
+		$scope.gridOptions.data = JSON.parse(message2);
+        console.log($scope.gridOptions);
 	});
+	$scope.goPath = function(path){
+		var url = "/contractDetails/" + path;
+	  	$location.path(url);
+	}
 	
 }]);
 
-pilotApp.controller('contractDetailsController', ['$scope', '$http' , function($scope, $http) {
+pilotApp.controller('contractDetailsController', ['$scope', '$http', '$location', '$routeParams' , function($scope, $http, $location, $routeParams) {
+
+	$scope.dispatchOrderId = $routeParams.id;
 
 	$http.get('data/contractHistoryResponse.json').success(function(reponse){
 		console.log(reponse);
+		
 		$scope.contractHisory = JSON.parse(reponse.result.message);
 		console.log($scope.contractHisory);
 	});
+	$http.get('data/dispatchOrderDetails.json').success(function(reponse){
+		console.log(reponse);
+		$scope.dispatchOrderDetails = JSON.parse(reponse.result.message);
+		console.log($scope.dispatchOrderDetails);
+	}); 
 	
 }]);
+
