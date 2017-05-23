ffc35205f912a0d1ff4fe6c86f8a40795f5837ca
cibilan <cibilanj@babajob.com>
2017-05-23 23:56:57 +0530

    para added to upload and download

--- app/contractDetailsController.js
+++ app/contractDetailsController.js
@@ -11,16 +11,16 @@ angular.module('pilotApp').controller('contractDetailsController', ['$scope', '$
 		var arg = [$scope.dispatchOrderId];
 		var arg2 = ["transaction",$scope.dispatchOrderId];	      
 
-    	$scope.param = para.myFunc("query","read",$scope.user,arg); 
-    	$scope.param2 = para.myFunc("query","getHistory",$scope.user,arg2)         
+    	var param = para.myFunc("query","read",$scope.user,arg); 
+    	var param2 = para.myFunc("query","getHistory",$scope.user,arg2)         
     
 
-		$http.post($scope.user.url,$scope.param2).success(function(reponse){
+		$http.post($scope.user.url,param2).success(function(reponse){
 		$scope.contractHisory = JSON.parse(reponse.result.message);
 		console.log($scope.contractHisory);
 		});
 
-		$http.post($scope.user.url,$scope.param).success(function(reponse){
+		$http.post($scope.user.url,param).success(function(reponse){
 		$scope.dispatchOrderDetails = JSON.parse(reponse.result.message);
 		console.log($scope.dispatchOrderDetails);
 		});
@@ -30,10 +30,9 @@ angular.module('pilotApp').controller('contractDetailsController', ['$scope', '$
 	function update(){
 
 		var arg = [$scope.dispatchOrderDetails.dispatchOrderId,$scope.dispatchOrderDetails.stage,$scope.dispatchOrderDetails.customer,$scope.dispatchOrderDetails.transporter,$scope.dispatchOrderDetails.seller,$scope.dispatchOrderDetails.assetIDs,$scope.dispatchOrderDetails.asnNumber,$scope.dispatchOrderDetails.source,$scope.dispatchOrderDetails.shipmentType,$scope.dispatchOrderDetails.contractType,$scope.dispatchOrderDetails.deliveryTerm,$scope.dispatchOrderDetails.dispatchDate,$scope.dispatchOrderDetails.transporterRef,$scope.dispatchOrderDetails.loadingType,$scope.dispatchOrderDetails.vehicleType,$scope.dispatchOrderDetails.weight,$scope.dispatchOrderDetails.consignment,$scope.dispatchOrderDetails.quantity,$scope.dispatchOrderDetails.partNumber,$scope.dispatchOrderDetails.partName,$scope.dispatchOrderDetails.orderRefNum,$scope.dispatchOrderDetails.createdOn,$scope.dispatchOrderDetails.documentID1,$scope.dispatchOrderDetails.documentID2,$scope.dispatchOrderDetails.documentID3,$scope.dispatchOrderDetails.documentID4,$scope.dispatchOrderDetails.dropDescription,$scope.dispatchOrderDetails.deliverydescription,$scope.dispatchOrderDetails.inTransitDisptachOfficerSigned,$scope.dispatchOrderDetails.inTransitTransporterSigned,$scope.dispatchOrderDetails.transactionDescription];
-		$scope.param = para.myFunc("invoke","updateDispatchOrder",$scope.user,arg);
-		$http.post($scope.user.url,$scope.param).success(function(reponse){
-			console.log($scope.param);
-			refresh();
+		var param = para.myFunc("invoke","updateDispatchOrder",$scope.user,arg);
+		$http.post($scope.user.url,param).success(function(reponse){
+			console.log($scope.param);			
 		});
 
 	}
@@ -57,37 +56,23 @@ angular.module('pilotApp').controller('contractDetailsController', ['$scope', '$
 		      var result = "";
 		      for(var i = 0; i < bytes.length; i++){
 		        result+= (String.fromCharCode(bytes[i]));
-		      };         
-		      var parameter2 = JSON.stringify({"jsonrpc": "2.0",
-		        "method": "invoke",
-		        "params": {
-		          "chaincodeID": {
-		            "name": $scope.user.cc
-		          },
-		          "ctorMsg": {
-		            "function": "createDocument",
-		            "args": [
-		              $scope.dispatchOrderId + id,
-		              fileName,
-		              fileType,
-		              result
-		            ]
-		          },
-		          "secureContext": $scope.user.userName
-		        },
-		        "id": "2"
-		      });
+		      };
+		      var arg = [$scope.dispatchOrderId + id,fileName,fileType,result];
 
-		     //console.log(parameter2);
+		      var parameter2 = para.myFunc("invoke","createDocument",$scope.user,arg);         
+
+		     console.log(parameter2);
 
 		     $scope.dispatchOrderDetails.documentID1 = $scope.dispatchOrderId + id;
 		     $scope.dispatchOrderDetails.transactionDescription = id + " Uploaded";
 
-		     update();
+		     
 
-	        /*$http.post('http://localhost:7050/chaincode',parameter2).success(function(response) {
-            	console.log($scope.dispatchOrderDetails);
-      		}); */
+	        $http.post($scope.user.url,parameter2).success(function(response) {
+            	update();
+
+		     	refresh();
+      		});
 
 		    }
 		  }
@@ -95,37 +80,19 @@ angular.module('pilotApp').controller('contractDetailsController', ['$scope', '$
 
 	$scope.downFile = function(){
 
-			var parameter3 = JSON.stringify({
-				  "jsonrpc": "2.0",
-				  "method": "query",
-				  "params": {
-				    "type": 1,
-				    "chaincodeID": {
-				      "name": $scope.user.cc
-				    },
-				    "ctorMsg": {
-				      "function": "getDocuments",
-				      "args": [
-				        "document",
-				         $scope.dispatchOrderDetails.documentID1
-				        ]
-				    },
-				    "secureContext": $scope.user.userName
-				  },
-				  "id": 3
-				}
-				);
+			var arg = ["document",$scope.dispatchOrderDetails.documentID1];
+
+			var parameter3 = para.myFunc("query","getDocuments",$scope.user,arg);
+
 			console.log(parameter3);
-			$http.post('http://localhost:7050/chaincode',parameter3).success(function(response) {
+			$http.post($scope.user.url,parameter3).success(function(response) {
             console.log(response);
-            var result = "" ;         
-            result = response.result.message;
-            result2 = JSON.parse(result);
-            console.log(result2);
-            result3 = result2[0].documentString;
+                  
+            var result = response.result.message;
+            var result2 = JSON.parse(result); 
+            var result3 = result2[0].documentString;
 
 
-            console.log(result3);
 		      var arr=[];
 		      for(var i=0; i<result3.length; i++) {
 		            arr.push(result3.charCodeAt(i))
